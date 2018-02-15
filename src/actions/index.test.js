b51a49a4d11ad185a8281533690adc1575131705
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-14 19:33:04 -0700

    gitignore

---
+++ src/actions/index.test.js
@@ -0,0 +1,14 @@
+import * as actions from './index';
+
+describe('actions', () => {
+  describe('addLocation', () => {
+    it('should return false', () => {
+      expect(true).toEqual(false)
+    })
+
+    it('should return a type of ADD_LOCATION with a single location', () => {
+      const location = {};
+
+    })
+  })
+})
