ffba757704d530e47efc1ab1991e510220541e0a
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-15 14:30:16 -0700

    Start importing components into each other on Main page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,11 +13,11 @@ class App extends Component {
         <Header />
         <Route exact path='/' component={Welcome} />
         <Route path='/home' component={Banner} />
-        <Route exact path='/home' render={() => (<Main title='This Week'/>)} />
-        <Route exact path='/home/music' render={() => (<Main title='MUSIC'/>)} />
-        <Route exact path='/home/food' render={() => (<Main title='FOOD'/>)} />
-        <Route exact path='/home/culture' render={() => (<Main title='CULTURE'/>)} />
-        <Route exact path='/home/nightlife' render={() => (<Main title='NIGHTLIFE'/>)} />
+        <Route exact path='/home' render={() => (<Main type='This Week'/>)} />
+        <Route exact path='/home/music' render={() => (<Main type='MUSIC'/>)} />
+        <Route exact path='/home/food' render={() => (<Main type='FOOD'/>)} />
+        <Route exact path='/home/culture' render={() => (<Main type='CULTURE'/>)} />
+        <Route exact path='/home/nightlife' render={() => (<Main type='NIGHTLIFE'/>)} />
       </div>
     );
   }
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NavCategories from '../../containers/NavCategories/NavCategories';
 import './Banner.css';
 
 
@@ -7,6 +8,7 @@ const Banner = () => {
   return (
     <section className="banner">
       I'm a BANNER!
+      <NavCategories /> 
     </section>
   )
 }
