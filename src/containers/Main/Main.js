0efc63d7cc741c355fe19f23835c1f0940c6ab51
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-16 16:24:08 -0700

    Write API calls for different categories and get them rendering on corresponding route

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,23 +1,61 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { cleanEventData, 
+  getMusicData, 
+  getFoodData, 
+  getCultureData, 
+  getNightlifeData 
+} from '../../helper';
+import { addMusic,
+  addFood,
+  addCulture,
+  addNightlife
+ } from '../../actions/';
 import Events from '../../components/Events/Events';
 import NavTime from '../NavTime/NavTime';
 import './Main.css';
 
-// Main.propTypes = {
-//   // music: PropTypes.array,
-//   // food: PropTypes.array,
-//   // culture: PropTypes.array,
-//   // nightlife: PropTypes.array
-// };
-
 class Main extends Component {
-  // on componentDidMount make the fetch calls for music, food, culture, nightlife
-  // componentDidMount = async() {
-  //   const musicResponse = await 
-  // }
+  componentDidMount() {
+    this.getAndStoreMusic();
+    this.getAndStoreFood();
+    this.getAndStoreCulture();
+    this.getAndStoreNightlife();  
+  }
+
+  getAndStoreMusic = async () => {
+    const { location, addMusic } = this.props;
+    const musicData = await getMusicData(location);
+    const cleanMusicData = cleanEventData(musicData);
+
+    addMusic(cleanMusicData);    
+  }
+
+  getAndStoreFood = async () => {
+    const { location, addFood } = this.props;
+    const foodData = await getFoodData(location);
+    const cleanFoodData = cleanEventData(foodData);
+
+    addFood(cleanFoodData);
+  }
+
+  getAndStoreCulture = async () => {
+    const { location, addCulture } = this.props;
+    const cultureData = await getCultureData(location);
+    const cleanCultureData = cleanEventData(cultureData);
+
+    addCulture(cleanCultureData);
+  }
+
+  getAndStoreNightlife = async () => {
+    const { location, addNightlife } = this.props;
+    const nightlifeData = await getNightlifeData(location);
+    const cleanNightlifeData = cleanEventData(nightlifeData);
+
+    addNightlife(cleanNightlifeData);
+  }  
 
   render() {
     const { events, music, food, culture, nightlife } = this.props;
@@ -27,7 +65,7 @@ class Main extends Component {
         I'm a Main!
         <h3>{this.props.type}</h3>
         <Route 
-          exact path='/home' 
+          exact path='/home/' 
           render={() => (<Events info={events} />)}
         />
         <Route 
@@ -51,12 +89,36 @@ class Main extends Component {
   }
 }
 
+Main.propTypes = {
+  location: PropTypes.string,
+  events: PropTypes.array,
+  music: PropTypes.array,
+  food: PropTypes.array,
+  culture: PropTypes.array,
+  nightlife: PropTypes.array,
+  addMusic: PropTypes.func,
+  addFood: PropTypes.func,
+  addCulture: PropTypes.func,
+  addNightlife: PropTypes.func
+};
+
 const mapStateToProps = (state) => ({
   events: state.events,
+  location: state.location,
   music: state.music,
   food: state.food,
   culture: state.culture,
   nightlife: state.nightlife
 })
 
-export default connect(mapStateToProps)(Main);
+const mapDispatchToProps = (dispatch) => ({
+  addMusic: events => dispatch(addMusic(events)),
+  addFood: events => dispatch(addFood(events)),
+  addCulture: events => dispatch(addCulture(events)),
+  addNightlife: events => dispatch(addNightlife(events))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
+
+
+        
