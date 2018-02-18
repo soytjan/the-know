cc93cc6c741b4fd96f57e064196696634e469349
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-17 17:39:48 -0700

    Write tests and TDD favorites reducer

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -2,6 +2,8 @@ export const favoritesReducer = (state = [], action) => {
   switch(action.type) {
     case 'ADD_FAVORITE':
       return [...state, action.event];
+    case 'REMOVE_FAVORITE':
+      return state.filter(event => event.title !== action.event.title);
     default:
       return state;
   }
--- src/reducers/favoritesReducer.test.js
+++ src/reducers/favoritesReducer.test.js
@@ -2,5 +2,25 @@ import { favoritesReducer } from './favoritesReducer';
 import * as actions from '../actions';
 
 describe('favoritesReducer', () => {
-  
+  it('should return the default state', () => {
+    const expected = [];
+
+    expect(favoritesReducer(undefined, {})).toEqual(expected);
+  })
+
+  it('ADD_FAVORITE should return the state with the favorited event added', () => {
+    const action = actions.addFavorite({ title: 'hello' });
+    const expected = [ {title: 'hello'} ];
+
+    expect(favoritesReducer(undefined, action)).toEqual(expected);
+  })
+
+  it('REMOVE_FAVORITE should return the state with the favorited event removed', () => {
+    const state = [{title: 'hello'}, {title: 'bye'}];
+
+    const action = actions.removeFavorite({ title: 'hello' });
+    const expected = [ {title: 'bye'} ];
+
+    expect(favoritesReducer(state, action)).toEqual(expected);
+  })
 })
