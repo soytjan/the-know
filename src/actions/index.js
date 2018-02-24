4d8eef9cabd61a3bd2e32c8581bdf0c8acaa2956
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-24 09:23:32 -0700

    Delete unnecessary actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,19 +5,6 @@ export const addCurrentLocation = coordinates => ({
   coordinates
 });
 
-export const currentLocationFetchData = () => {
-  return async (dispatch) => {
-    try {
-      const jsonResponse = await getGeoLocation();
-      const currentLocation = { coordinates: jsonResponse.location }
-      dispatch(addCurrentLocation(currentLocation));
-    } catch (err) {
-      dispatch(currentHasErrored(true));
-      throw err;
-    }  
-  }
-};
-
 export const currentHasErrored = (bool) => ({
   type: 'CURRENT_HAS_ERRORED',
   hasErrored: bool
@@ -27,11 +14,6 @@ export const addLocation = location => ({
   type: 'ADD_LOCATION',
   location
 });
-
-// export const addEvents = events => ({
-//   type: 'ADD_EVENTS',
-//   events
-// });
 
 export const addEvents = (events, category) => ({
   type: 'ADD_EVENTS',
@@ -44,19 +26,9 @@ export const updateEvents = event => ({
   event
 });
 
-export const addMusic = events => ({
-  type: 'ADD_MUSIC',
-  events
-});
-
 export const updateMusic = event => ({
   type: 'UPDATE_MUSIC',
   event
-});
-
-export const addFood = events => ({
-  type: 'ADD_FOOD',
-  events
 });
 
 export const updateFood = event => ({
@@ -64,20 +36,11 @@ export const updateFood = event => ({
   event
 });
 
-export const addCulture = events => ({
-  type: 'ADD_CULTURE',
-  events
-});
-
 export const updateCulture = event => ({
   type: 'UPDATE_CULTURE',
   event
 });
 
-export const addNightlife = events => ({
-  type: 'ADD_NIGHTLIFE',
-  events
-});
 
 export const updateNightlife = event => ({
   type: 'UPDATE_NIGHTLIFE',
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,13 @@
 import { combineReducers } from 'redux';
 import { eventsReducer } from './eventsReducer';
-import { cultureReducer } from './cultureReducer';
 import { locationReducer } from './locationReducer';
 import { currentLocationReducer } from './currentLocationReducer';
-import { currentHasErroredReducer } from './currentHasErroredReducer';
-import { foodReducer } from './foodReducer';
-import { musicReducer } from './musicReducer';
-import { nightlifeReducer } from './nightlifeReducer';
 import { favoritesReducer } from './favoritesReducer';
 
 const rootReducer = combineReducers({
   currentLocation: currentLocationReducer,
-  currentHasErrored: currentHasErroredReducer,
   events: eventsReducer,
   location: locationReducer,
-  music: musicReducer,
-  food: foodReducer,
-  culture: cultureReducer,
-  nightlife: nightlifeReducer,
   favorites: favoritesReducer,
 });
 
