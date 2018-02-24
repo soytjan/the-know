5e351b4ccee6511af4753276b468bd3931ab2514
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-24 11:58:22 -0700

    Update favorites test with refactor, delete old favoritesReducer

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,14 +1,3 @@
-// export const favoritesReducer = (state = {}, action) => {
-//   switch(action.type) {
-//     case 'ADD_FAVORITE':
-//       return [...state, action.event];
-//     case 'REMOVE_FAVORITE':
-//       return state.filter(event => event.title !== action.event.title);
-//     default:
-//       return state;
-//   }
-// };
-
 export const favoritesReducer = (state = {}, action) => {
   switch(action.type) {
     case 'ADD_FAVORITE':
