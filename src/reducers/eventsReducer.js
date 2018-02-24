0ca90c6a615fe0d9175d1fbb6e8dd8ef8248f38c
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-24 11:25:50 -0700

    Refactor UPDATE_EVENTS case to make it more readable

--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -1,26 +1,10 @@
-// export const eventsReducer = (state = [], action) => {
-//   switch(action.type) {
-//     case 'ADD_EVENTS':
-//       return [...state, ...action.events];
-//     case 'UPDATE_EVENTS':
-//       return state.map(event => { 
-//         if (event.id === action.event.id) {
-//           return action.event;
-//         } 
-
-//         return event;
-//       })
-//     default:
-//       return state;
-//   }
-// };
-
 export const eventsReducer = (state = {}, action) => {
   switch(action.type) {
     case 'ADD_EVENTS':
       return {...state, [action.category]: action.events };
     case 'UPDATE_EVENTS':
-      return {...state, [action.event.category]: {...state[action.event.category], [action.event.id]: action.event}}
+      const { category, id } = action.event;
+      return {...state, [category]: {...state[category], [id]: action.event}}
     default: 
       return state;
   }
