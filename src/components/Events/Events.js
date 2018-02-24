a688f4ed9308cfbb3cd6fe3b1e5a2d17d1aedb31
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-24 16:03:53 -0700

    Write functions to filter through data depending on date, create a state with the array that needs to be mapped through

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -7,6 +7,17 @@ import EventCard from '../EventCard/EventCard';
 import './Events.css';
 
 class Events extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      events: []
+    }
+  }
+
+  componentDidMount() {
+    this.getFilteredEventsArray('all');
+  }
+
   genEventsArray = () => {
     const { info, type } = this.props;
     let events;
@@ -27,16 +38,57 @@ class Events extends Component {
       return convertObjToArray(events);
   }
 
+  filterEvents(eventArray, time) {
+    // move this to helper function
+    // in a month
+    const timeLimit = this.setTimeLimit(time);
+    const filtered = eventArray.filter(event => {
+      const eventDate = new Date(event.startTime);
+      return eventDate < timeLimit;
+    })
+
+    return filtered
+  }
+
+  setTimeLimit(time) {
+    const today = new Date();
+
+    switch(time) {
+      case 'week':
+        return today.setDate(today.getDate() + 7)
+      case 'month':
+        return (today.setMonth(today.getMonth() + 1))
+      default: 
+        return new Date(today.setFullYear(today.getFullYear() + 1))
+    }
+  }
+
+  getFilteredEventsArray = (time) => {
+    const eventArray = this.genEventsArray();
+    const filteredEvents = this.filterEvents(eventArray, time);
+
+    this.setState({events: filteredEvents})
+  }
+
+  renderEvents() {
+
+  }
+
   render() {
     const { onFavorite } = this.props;
-    const eventArray = this.genEventsArray();
-    const renderedEvents = eventArray.map(event => {
+    // const eventArray = this.genEventsArray();
+    // const filteredEvents = this.filterEvents(eventArray, 'all');
+
+
+    const renderedEvents = this.state.events.map(event => {
       return <EventCard event={event} onFavorite={onFavorite} /> 
     })
 
     return (
       <section className='Events'>
-        I'm the Events!
+        <button onClick={() => this.getFilteredEventsArray('week')}>This Week</button>
+        <button onClick={() => this.getFilteredEventsArray('month')}>This Month</button>
+        <button onClick={() => this.getFilteredEventsArray('all')}>All Upcoming</button>
         { renderedEvents }
       </section>
     )
