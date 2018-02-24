a01853e612b2f3935e630595823a4cb726dc3975
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-23 18:25:06 -0700

    Comment out handleFavorites function and start changing eventsReducer

--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -1,17 +1,26 @@
-export const eventsReducer = (state = [], action) => {
-  switch(action.type) {
-    case 'ADD_EVENTS':
-      return [...state, ...action.events];
-    case 'UPDATE_EVENTS':
-      return state.map(event => { 
-        if (event.id === action.event.id) {
-          return action.event;
-        } 
+// export const eventsReducer = (state = [], action) => {
+//   switch(action.type) {
+//     case 'ADD_EVENTS':
+//       return [...state, ...action.events];
+//     case 'UPDATE_EVENTS':
+//       return state.map(event => { 
+//         if (event.id === action.event.id) {
+//           return action.event;
+//         } 
 
-        return event;
-      })
-    default:
+//         return event;
+//       })
+//     default:
+//       return state;
+//   }
+// };
+
+export const eventsReducer = (state = {}, action) => {
+  switch(action.type) {
+    case 'ADD_MUSIC':
+      return {...state, music: }
+    default: 
       return state;
   }
-};
+}
 
