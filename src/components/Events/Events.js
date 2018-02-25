1db5e7838d643874db0bb0bfbdded39f34faa13a
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-25 11:13:20 -0700

    Get the filtered events working so that user can favorite and filter

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -8,29 +8,13 @@ class Events extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      events: []
+      time: 'all'
     } 
   }
 
-  componentDidMount() {
-    const array = this.genEventsArray();
-
-    this.setState({events: array})
-  }
-
-  // componentWillReceiveProps(nextProps) {
-  //   if (this.props.info !== nextProps.info ) {
-  //     console.log('component will receive props')
-  //     const newEvents = convertObjToArray(nextProps.info)
-  //     console.log('newArray', newEvents)
-  //     this.setState({events: newEvents})
-  //     console.log('state was set')
-  //   }
-  // }
-
-  genEventsArray = () => {
+  convertEvents = () => {
     const { info, type } = this.props;
-    console.log(info)
+
     if (type === 'event') {
       const categories = Object.keys(info);
       return categories.reduce((eventsArr, category) => {
@@ -42,54 +26,35 @@ class Events extends Component {
       return convertObjToArray(info);
   }
 
-  filterEvents(eventArray, time) {
+  filterEvents() {
+    const { time } = this.state;
     const timeLimit = setTimeLimit(time);
-    const filtered = eventArray.filter(event => {
+    const events = this.convertEvents();
+
+    return events.filter(event => {
       const eventDate = new Date(event.startTime);
       return eventDate < timeLimit;
     })
-
-    return filtered
-  }
-
-  renderEvents = (time) => {
-    const { onFavorite } = this.props;
-    const filteredEvents = this.getFilteredEventsArray(time)
-
-    const events = filteredEvents.map(event => {
-      return <EventCard event={event} onFavorite={onFavorite} /> 
-    })
-
-    return events;
-  }
-
-  getFilteredEventsArray = (time) => {
-    const eventArray = this.genEventsArray();
-    const filteredEvents = this.filterEvents(eventArray, time);
-   
-    this.setState({events: filteredEvents})
-    // return filteredEvents;
   }
 
   render() {
     const { onFavorite } = this.props;
-    const renderedEvents = this.state.events.map(event => {
-      return <EventCard event={event} onFavorite={onFavorite} /> 
-    })
-    // const renderedEvents = this.renderEvents('all');
+    const events = this.filterEvents();
+    const renderedEvents = events.map(event => <EventCard event={event} onFavorite={onFavorite} key={event.id}/> )
+
 
     return (
       <section className='Events'>
-        <nav className='nav-time-container'>
+        <nav className='nav-time-container'> 
           <button 
-            onClick={() => this.getFilteredEventsArray('week')}>This Week
+            onClick={() => this.setState({ time: 'week' })}>This Week
           </button>
           <button 
-            onClick={() => this.getFilteredEventsArray('month')}>
+            onClick={() => this.setState({ time: 'month' })}>
             This Month
           </button>
           <button 
-            onClick={() => this.getFilteredEventsArray('all')}>All Upcoming
+            onClick={() => this.setState({ time: 'all' })}>All Upcoming
           </button>
         </nav>
         { renderedEvents }
